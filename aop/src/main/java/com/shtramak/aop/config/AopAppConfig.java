154f32e930765a4d46898e87c096b750204b206d
shtramak <shtramak@gmail.com>
2018-09-19 12:49:42 +0300

    add configuration class AopAppConfig with @EnableAspectJAutoProxy

---
+++ aop/src/main/java/com/shtramak/aop/config/AopAppConfig.java
@@ -0,0 +1,11 @@
+package com.shtramak.aop.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+@ComponentScan("com.shtramak.aop")
+public class AopAppConfig {
+}
