be501d2d0cafac0734829e43197416b3673e816d
shtramak <shtramak@gmail.com>
2018-09-20 10:56:32 +0300

    update AppRunner, aspect matches all addAccount() methods call

--- aop/src/main/java/com/shtramak/aop/AppRunner.java
+++ aop/src/main/java/com/shtramak/aop/AppRunner.java
@@ -2,6 +2,7 @@ package com.shtramak.aop;
 
 import com.shtramak.aop.config.AopAppConfig;
 import com.shtramak.aop.dao.AccountDao;
+import com.shtramak.aop.dao.MembershipDao;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppRunner {
@@ -9,7 +10,8 @@ public class AppRunner {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopAppConfig.class)) {
             AccountDao accountDao = context.getBean(AccountDao.class);
             accountDao.addAccount();
-            accountDao.addAccount();
+            MembershipDao membershipDao = context.getBean("membershipDao",MembershipDao.class);
+            membershipDao.addAccount();
         }
     }
 }
