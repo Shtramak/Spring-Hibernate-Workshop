f942be15303c7344e6c159312f6dba62c9b22340
shtramak <shtramak@gmail.com>
2018-09-20 11:29:04 +0300

    add Account entity class and parametrize AccountDao.addAccount() with it
    @Before("execution(* add*())") mathches only addAddAccount() in MembershipDao

--- aop/src/main/java/com/shtramak/aop/AppRunner.java
+++ aop/src/main/java/com/shtramak/aop/AppRunner.java
@@ -3,13 +3,14 @@ package com.shtramak.aop;
 import com.shtramak.aop.config.AopAppConfig;
 import com.shtramak.aop.dao.AccountDao;
 import com.shtramak.aop.dao.MembershipDao;
+import com.shtramak.aop.entity.Account;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AppRunner {
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopAppConfig.class)) {
             AccountDao accountDao = context.getBean(AccountDao.class);
-            accountDao.addAccount();
+            accountDao.addAccount(new Account());
             MembershipDao membershipDao = context.getBean("membershipDao",MembershipDao.class);
             membershipDao.addAccount();
         }
--- aop/src/main/java/com/shtramak/aop/dao/AccountDao.java
+++ aop/src/main/java/com/shtramak/aop/dao/AccountDao.java
@@ -1,10 +1,11 @@
 package com.shtramak.aop.dao;
 
+import com.shtramak.aop.entity.Account;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AccountDao {
-    public void addAccount(){
+    public void addAccount(Account account){
         System.out.println(this.getClass()+" Making some stuff with DB\n");
     }
 }
