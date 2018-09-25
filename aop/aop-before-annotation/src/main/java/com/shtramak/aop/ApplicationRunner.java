60686280c7bf0a898a9ab3d2ae294a9820172ee6
shtramak <shtramak@gmail.com>
2018-09-25 17:02:15 +0300

    create submodule aop-before-annotation and move references files

--- aop/aop-before-annotation/src/main/java/com/shtramak/aop/ApplicationRunner.java
+++ aop/aop-before-annotation/src/main/java/com/shtramak/aop/ApplicationRunner.java
@@ -6,7 +6,7 @@ import com.shtramak.aop.dao.MembershipDao;
 import com.shtramak.aop.entity.Account;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class AppRunner {
+public class ApplicationRunner {
     public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AopAppConfig.class)) {
             AccountDao accountDao = context.getBean(AccountDao.class);
